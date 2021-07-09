d1ba5fdb9902befecbe5e893b6558a30b809d6dd
4bpb <blakebein@gmail.com>
2021-07-09 15:19:51 -0700

    Added Distance Calculation

--- shared/Physics.ts
+++ shared/Physics.ts
@@ -8,3 +8,16 @@ export function checkCircleCollision(
 ): boolean {
     return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2)) <= r1 + r2;
 }
+
+
+
+export function checkPlayerDistance(
+    x1: number,
+    y1: number,
+    r1: number,
+    x2: number,
+    y2: number,
+    r2: number
+) {
+     return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2)) 
+}
