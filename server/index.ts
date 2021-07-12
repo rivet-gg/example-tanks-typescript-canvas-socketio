f48b7f201dc9276f537407804c7dc5244b266cc5
Nathan Flurry <developer@nathanflurry.com>
2021-07-12 21:41:09 +0000

    Improved port killing to happen every time you start the server

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,17 @@
 import { Server } from "./Server";
 import { Server as SocketServer } from "socket.io";
+import * as killOthers from "kill-port";
 
-let port = parseInt(process.env.PORT) || 5000;
-let socketServer = new SocketServer(port, {
-    cors: {
-        origin: "*",
-    },
-});
-Server.shared = new Server(socketServer);
-console.log(`Listening on port ${port}`);
+async function main() {
+    let port = parseInt(process.env.PORT) || 5000;
+    await killOthers(port, "tcp"); // Kill any orphan processes using port 5000
+    let socketServer = new SocketServer(port, {
+        cors: {
+            origin: "*",
+        },
+    });
+    Server.shared = new Server(socketServer);
+    console.log(`Listening on port ${port}`);
+}
+
+main();
