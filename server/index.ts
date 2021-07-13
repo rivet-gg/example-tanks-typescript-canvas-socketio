2ac71308a83403d739f002395157a5e0b4b88f54
Nathan Flurry <developer@nathanflurry.com>
2021-07-13 19:09:55 +0000

    WIP run task

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,11 @@
 import { Server } from "./Server";
 import { Server as SocketServer } from "socket.io";
-import * as killOthers from "kill-port";
 
-async function main() {
-    let port = parseInt(process.env.PORT) || 5000;
-    await killOthers(port, "tcp"); // Kill any orphan processes using port 5000
-    let socketServer = new SocketServer(port, {
-        cors: {
-            origin: "*",
-        },
-    });
-    Server.shared = new Server(socketServer);
-    console.log(`Listening on port ${port}`);
-}
-
-main();
+let port = parseInt(process.env.PORT) || 5000;
+let socketServer = new SocketServer(port, {
+    cors: {
+        origin: "*",
+    },
+});
+Server.shared = new Server(socketServer);
+console.log(`Listening on port ${port}`);
